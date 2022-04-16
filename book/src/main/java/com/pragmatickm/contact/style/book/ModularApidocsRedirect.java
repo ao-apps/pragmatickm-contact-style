8065835730a38381a1725f1df4c6577fb151b0ea
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:03:16 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- book/src/main/java/com/pragmatickm/contact/style/book/ModularApidocsRedirect.java
+++ book/src/main/java/com/pragmatickm/contact/style/book/ModularApidocsRedirect.java
@@ -1,6 +1,6 @@
 /*
  * pragmatickm-contact-style - Default style for contacts nested within SemanticCMS pages and elements.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with pragmatickm-contact-style.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.pragmatickm.contact.style.book;
 
 import com.aoapps.net.URIParametersUtils;
